a34c46e79e4d4e547c4d275b7aa45755f23f0b02
Zainsye <zainulabuddin.syed@ontariotechu.net>
2025-11-17 17:02:24 -0500

    Extend the API

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello():
     return jsonify(message="Hello, World!")
 
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json(force=True)
+    return jsonify(data), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,4 +1,12 @@
+from flask import json
 import app
+
+def test_echo():
+    client = app.app.test_client()
+    payload = {"msg": "ping"}
+    response = client.post('/echo', json=payload)
+    assert response.status_code == 201
+    assert response.get_json() == payload
 
 def test_hello():
     client = app.app.test_client()
